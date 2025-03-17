8b335c697c7d6c319f4b019185699a1ceeb792b7
dheeraj1566 <dheerajjangid013@gmail.com>
2025-03-17 18:37:38 +0530

    cors error

--- back/index.js
+++ back/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/authRouts.js";
 
+
 dotenv.config();
 
 const app = express();
@@ -11,22 +12,20 @@ const PORT = process.env.PORT || 5000;
 
 
 const corsOptions = {
-  origin: ["https://inventory-management-mu-hazel.vercel.app/"],
-  methods: "GET,POST,PUT,DELETE",
+  origin: "https://inventory-management-mu-hazel.vercel.app", 
+  methods: "GET, POST, PUT, DELETE",
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); 
 app.use(express.json());
-
 
 app.use("/auth", authRoutes);
 
-
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log(" MongoDB Connected"))
+  .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(" MongoDB Connection Error:", err));
 
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
