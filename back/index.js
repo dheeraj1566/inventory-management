14afa86fe21cf9def29399230a03d5e322e855d8
dheeraj1566 <dheerajjangid013@gmail.com>
2025-03-17 18:16:32 +0530

    link backend or frontend

--- back/index.js
+++ back/index.js
@@ -2,20 +2,31 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import authRoutes from "./routes/authRouts.js"
+import authRoutes from "./routes/authRouts.js";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+const corsOptions = {
+  origin: ["https://inventory-management-mu-hazel.vercel.app/"],
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
+
 
 app.use("/auth", authRoutes);
 
+
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(" MongoDB Connected"))
+  .catch((err) => console.log(" MongoDB Connection Error:", err));
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
