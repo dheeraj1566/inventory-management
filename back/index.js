41c5b89e543b89a5c3f41cfd0fd2b6feaa029198
dheeraj1566 <dheerajjangid013@gmail.com>
2025-03-18 11:04:25 +0530

    login error ..3

--- back/index.js
+++ back/index.js
@@ -12,13 +12,15 @@ const PORT = process.env.PORT || 5000;
 
 
 const corsOptions = {
-  origin: "https://inventory-management-mu-hazel.vercel.app", 
-  methods: "GET, POST, PUT, DELETE",
+  origin: ["https://inventory-management-mu-hazel.vercel.app"], 
+  methods: "GET, POST,DELETE",
   credentials: true,
+  optionsSuccessStatus: 200, 
 };
 
 app.use(cors(corsOptions)); 
 app.use(express.json());
+
 
 app.use("/auth", authRoutes);
 
