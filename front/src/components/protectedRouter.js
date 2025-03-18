6b81e51b22f7e113e0c0bae696b3c0439071909c
dheeraj1566 <dheerajjangid013@gmail.com>
2025-03-18 12:42:54 +0530

    login error ..15

--- front/src/components/protectedRouter.js
+++ front/src/components/protectedRouter.js
@@ -1,10 +1,12 @@
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";\
+import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
-
+const navigate = useNavigate();
   if (!token) {
-    return <Navigate to="/login" replace />;
+    // return <Navigate to="/login" replace />;
+    navigate("/login")
   }
 
   return children;
