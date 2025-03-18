65cdf838a8cd57f43f744d7a6ce39317cf26338a
dheeraj1566 <dheerajjangid013@gmail.com>
2025-03-18 15:40:54 +0530

    update vercel-json according vercel 4

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,8 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist", // Ensure Vite outputs the build to "dist"
-  },
-  base: "./", // Ensures relative paths for assets
 });
