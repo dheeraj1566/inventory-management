e81da9c543b51390fcc99e49afb1ade27d4b2d06
dheeraj1566 <dheerajjangid013@gmail.com>
2025-03-20 12:09:47 +0530

    creat custom hook for user authentication 2

--- front/AxiosConfig.js
+++ front/AxiosConfig.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const Instance = axios.create({
   baseURL: "https://inventory-management-8mn8.onrender.com",
+  // baseURL : `http://localhost:8080`,
   headers: {
     "Content-Type": "application/json",
   },
