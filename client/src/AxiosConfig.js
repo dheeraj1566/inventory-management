482a74ac4980ed4f03baba670f9c4c3f7d647779
Priyanshiag1812 <priyanshiagarwal34269@iisuniv.ac.in>
2025-03-25 17:43:00 +0530

    updated code 3

--- client/src/AxiosConfig.js
+++ client/src/AxiosConfig.js
@@ -1,1 +1,8 @@
+import axios from "axios";
 
+const Instance = axios.create({
+    baseURL: "https://inventory-management-8mn8.onrender.com",
+    withCredentials:true,
+});
+
+export default Instance;
